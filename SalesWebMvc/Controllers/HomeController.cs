7512a9d1b9122984b2b7725ee41cdcd7af37966f
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2020-07-02 12:38:42 -0300

    Primeiro teste no Controller

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicação de vendas";
+            ViewData["Autor"] = "Eduardo Martiniano";
 
             return View();
         }
